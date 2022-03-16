8393c78a52c444f7fdacbd913b9e8afe0ae0be66
Dave Sanders <davepaulsanders@gmail.com>
2022-03-16 16:27:45 -0400

    Local storage updates, currently deletes old task if there is already one listed at that hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 const today = luxon.DateTime.now();
 $("#currentDay").text(today.toLocaleString());
 
+function test(event) {
+  // find focused element
+}
 function addEvent() {
   $(this).replaceWith(
-    "<textarea class='task m-0 bg-success col-9'></textarea>"
+    "<textarea onblur = 'test()'class='task m-0 bg-success col-9'></textarea>"
   );
   $(".task").focus();
 }
+
+// If the user leaves the block without saving task, empty block
 
 function saveEvent(event) {
   const eventBlock = $(this).siblings(".task");
@@ -18,13 +23,39 @@ function saveEvent(event) {
       task +
       "</div>"
   );
-  // turn the event into a paragraph or something
-  // get local storage
-  // If it's in there, overwrite
-  //If it's not, add it
+
+  const newTask = {
+    hour,
+    task,
+  };
+  let currentTasks = JSON.parse(localStorage.getItem("tasks"));
+  let updatedTasks;
+
+  // if that timeslot already contains something, delete it and store new one
+  for (x in currentTasks) {
+    if (currentTasks[x].hour === newTask.hour) {
+      updatedTasks = currentTasks.filter((y) => y !== currentTasks[x]);
+      updatedTasks.push(newTask);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      return;
+    }
+  }
+  if (!currentTasks) {
+    const taskArr = [];
+    taskArr.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(taskArr));
+    return;
+  }
+  currentTasks.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(currentTasks));
 }
+// turn the event into a paragraph or something
+// get local storage
+// If it's in there, overwrite
+//If it's not, add it
+
 const timeBlocksContainer = $(".time-blocks-container");
 timeBlocksContainer.on("click", ".eventblock", addEvent);
-
+//timeBlocksContainer.on("blur", ".task", test);
 const saveButton = $(".btn");
 saveButton.on("click", saveEvent);
